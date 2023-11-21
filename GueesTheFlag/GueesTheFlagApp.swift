789a7badbc194e1736a6c33417fa19753711ece1
Furkan Cingöz <138152979+furkanwithcode@users.noreply.github.com>
2023-11-22 00:18:02 +0300

    Initial Commit

---
+++ GueesTheFlag/GueesTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GueesTheFlagApp.swift
+//  GueesTheFlag
+//
+//  Created by Furkan Cingöz on 22.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct GueesTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
